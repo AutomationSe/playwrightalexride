8a7d042d366bd4a878697b25954b848b65137034
senel <senel.qa.emg@gmail.com>
2025-11-09 16:18:02 +0530

    apitest

--- tests/apitest.spec.ts
+++ tests/apitest.spec.ts
@@ -10,10 +10,11 @@
 
 import { test,expect } from "@playwright/test";
 import pageTwoAllUsers from '../test-data/reqs-userspageres.json';
+import { request } from "https";
 
 test.describe.only("Api Testing", ()=>{
     
-        test('Get all users', async({request})=>{
+    test('Get all users', async({request})=>{
 
         // Make GET request
         const response = await request.get('https://reqres.in/api/users?page=2');
@@ -30,4 +31,87 @@ test.describe.only("Api Testing", ()=>{
         expect(body).toEqual(pageTwoAllUsers);
 
     })
+
+    test('Get single user', async({request})=>{
+
+        // Make GET request
+        const response = await request.get('https://reqres.in/api/users/2');
+        const body = await response.json();
+        // Check status
+        // console.log(response)
+        // console.log(response.status())
+        // console.log(await response.json())
+        // expect(response.status()).toBe(200);
+        
+        console.log(body);
+
+        // Parse JSON body
+        
+        expect(body.data.id).toBe(2);
+        expect(body.data.email).toBe("janet.weaver@reqres.in");
+        expect(body.data.first_name).toBe("Janet");
+        expect(body.data.last_name).toBe("Weaver");
+        expect(body.data.avatar).toBe("https://reqres.in/img/faces/2-image.jpg");
+
+        // expect(body).toEqual(pageTwoAllUsers.data[1]);
+    });
+
+    test("Post - create new user", async({request})=>{
+
+        const newUser = {
+            name: "newstar",
+            job: "leader"
+        };
+        const response = await request.post('https://reqres.in/api/users', {
+            data: newUser,
+            headers: {
+                'x-api-key': 'reqres-free-v1'
+            }
+        });
+
+        expect(response.status()).toBe(201);
+        const body = await response.json();
+        console.log(body);
+        expect(body.name).toBe(newUser.name);
+        expect(body.job).toBe("qa engineer");
+
+    })
+
+
+      test('Update a User', async ({ request }) => {
+
+        const response = await request.put('https://reqres.in/api/users/2', {
+        data: {
+            name: 'Neo',
+            job: 'the One'
+        },
+        headers: {
+            'x-api-key': 'reqres-free-v1'
+        }
+        });
+
+        const body = await response.json();
+        expect(response.status()).toBe(200);
+        expect(body.name).toBe('Neo');
+        expect(body.job).toBe('the One');
+
+           
+    });
+
+
+    test('Delete a User', async({request})=>{
+
+            const response = await request.put('https://reqres.in/api/users/2',
+                {
+                    headers: {
+                            'x-api-key': 'reqres-free-v1'
+                             }       
+
+    })
+
+    console.log(response.status());
+    expect(response.status()).toBe(204)
+
+    })
+
 })
